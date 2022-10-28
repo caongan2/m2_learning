e78bb40a13d1eb90e2bee9825834859726d9966c
ngan_ct <nganct@niw.com.vn>
2022-10-28 08:56:02 +0700

    first commit

---
+++ app/code/Magento/Learning/Controller/ActionName/Index.php
@@ -0,0 +1,32 @@
+<?php
+namespace Magento\Learning\Controller\ActionName;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+    /**
+     * View page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        die('12121212121');
+        return $this->_pageFactory->create();
+    }
+}
